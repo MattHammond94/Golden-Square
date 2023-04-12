7d365310c4d89976edf7cb84e224126b5b71a343
Matt Hammond <mjhammond94@gmail.com>
2023-04-12 09:28:19 +0100

    String Builder Spec Updated

--- testing_bites/spec/string_builder_spec.rb
+++ testing_bites/spec/string_builder_spec.rb
@@ -14,7 +14,24 @@ describe 'StringBuilder' do
   end
 
   it 'Should return a string when output called after one add' do
-    
+    string = StringBuilder.new
+    string.add("Virgil")
+    expect(string.output).to eq "Virgil"
+  end
+
+  it 'Should return a combined string when output called after multiple adds' do
+    string = StringBuilder.new
+    string.add("Virgil")
+    string.add("Van")
+    string.add("Dijk")
+    expect(string.output).to eq "VirgilVanDijk"
+  end
+
+  it 'Should return an empty string when output called on no adds' do
+    expect(StringBuilder.new.output).to eq ""
+  end
+
+  it 'Should return a 0 when size called on no adds' do
+    expect(StringBuilder.new.size).to eq 0
   end
 end
-
