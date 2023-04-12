d68a4e839e95437ad2970318bb27c01b94e342bf
Matt Hammond <mjhammond94@gmail.com>
2023-04-12 19:52:11 +0100

    Code refactor

--- my_project/lib/make_snippet.rb
+++ my_project/lib/make_snippet.rb
@@ -1,8 +1,4 @@
 def make_snippet(string)
   arr = string.split(" ")
-  if arr.size < 5 
-    string
-  else
-    "#{arr[0..4].join(" ")}..."
-  end
+  arr.size <= 5 ? string : "#{arr[0..4].join(" ")}..."
 end
