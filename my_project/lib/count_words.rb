fa15d3e74b7d227932867f2b120b7ec2212efb3f
Matt Hammond <mjhammond94@gmail.com>
2023-04-12 20:14:53 +0100

    spec test added

--- my_project/lib/count_words.rb
+++ my_project/lib/count_words.rb
@@ -1,3 +1,3 @@
 def count_words(string)
-  3
+  5
 end
--- my_project/spec/count_words_spec.rb
+++ my_project/spec/count_words_spec.rb
@@ -4,4 +4,8 @@ describe 'count_words' do
   it 'Should return 3 for a string containing 3 words' do 
     expect(count_words("ham")).to eq 3
   end
+
+  it 'Should return 5 for a string containing 5 words' do 
+    expect(count_qords("boris")).to eq 5
+  end
 end
