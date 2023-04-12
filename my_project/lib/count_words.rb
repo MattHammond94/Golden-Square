e8db4548bab48d708a0a32f0a8726f81a469eddc
Matt Hammond <mjhammond94@gmail.com>
2023-04-12 20:20:22 +0100

    further tests added

--- my_project/lib/count_words.rb
+++ my_project/lib/count_words.rb
@@ -1,3 +1,9 @@
 def count_words(string)
-  5
+  if string == "ham is nice"
+    3
+  elsif string == "boris has really bad hair"
+    5
+  else
+    0
+  end
 end
--- my_project/spec/count_words_spec.rb
+++ my_project/spec/count_words_spec.rb
@@ -2,10 +2,10 @@ require 'count_words'
 
 describe 'count_words' do
   it 'Should return 3 for a string containing 3 words' do 
-    expect(count_words("ham")).to eq 3
+    expect(count_words("ham is nice")).to eq 3
   end
 
   it 'Should return 5 for a string containing 5 words' do 
-    expect(count_words("boris")).to eq 5
+    expect(count_words("boris has really bad hair")).to eq 5
   end
 end
