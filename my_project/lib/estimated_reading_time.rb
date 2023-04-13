94725b1b1c0afe0dfdc59912f6e9205c52b86667
Matt Hammond <mjhammond94@gmail.com>
2023-04-13 09:30:17 +0100

    all example tests added to spec

--- my_project/lib/estimated_reading_time.rb
+++ my_project/lib/estimated_reading_time.rb
@@ -1,3 +1,4 @@
 def estimated_reading_time(text)
+  fail "You have not entered a valid amount of words" if text.nil?
   text.to_f / 200
 end
--- my_project/spec/estimated_reading_time_spec.rb
+++ my_project/spec/estimated_reading_time_spec.rb
@@ -12,4 +12,16 @@ describe 'EstimatedReadingTime' do
   it 'Should return 0.5 for a total of 100 words' do
     expect(estimated_reading_time(100)).to eq 0.5
   end
+
+  it 'Should return 0.1 for a total of 20 words' do
+    expect(estimated_reading_time(20)).to eq 0.1
+  end
+
+  it 'Should return a 0 for no words' do
+    expect(estimated_reading_time(0)).to eq 0
+  end
+
+  it 'Should return an error if nothing is passed as an arg' do
+    expect { estimated_reading_time(nil) }.to raise_error "You have not entered a valid amount of words"
+  end
 end
