9e7a3f5831850d0339c1cbe0096b1319d5b00d49
Matt Hammond <mjhammond94@gmail.com>
2023-04-13 10:38:20 +0100

    further tests added - program updated to account for minutes and seconds individually

--- my_project/lib/estimated_reading_time.rb
+++ my_project/lib/estimated_reading_time.rb
@@ -1,8 +1,10 @@
 def estimated_reading_time(text)
   fail "You have not entered a valid amount of words" if text.nil? 
-  if text < 200 
-    (text.to_f / 200) * 60
-  else 
-    text.to_f / 200
+  if text == 0
+    0
+  elsif text <= 200 
+    "#{((text.to_f / 200) * 60).to_i} seconds"
+  else
+    "#{text % 200 == 0 ? text / 200 : text.to_f / 200} minutes to read"
   end
 end
--- my_project/spec/estimated_reading_time_spec.rb
+++ my_project/spec/estimated_reading_time_spec.rb
@@ -2,21 +2,23 @@ require 'estimated_reading_time'
 
 describe 'EstimatedReadingTime' do
 
-  it 'Should return 1 for a total of 200 words' do
-    expect(estimated_reading_time(200)).to eq 1
+  context 'Word counts greater than 200' do
+    it 'Should return a value in minutes for word counts higher than 200' do
+      expect(estimated_reading_time(250)).to eq "1.25 minutes to read"
+      expect(estimated_reading_time(400)).to eq "2 minutes to read"
+      expect(estimated_reading_time(500)).to eq "2.5 minutes to read"
+      expect(estimated_reading_time(1000)).to eq "5 minutes to read"
+    end
   end
 
-  it 'Should return 2 for a total of 400 words' do
-    expect(estimated_reading_time(400)).to eq 2
-  end
-
-  context 'word counts less than 200' do
+  context 'word counts less than or equal to 200' do
     it 'Should return a value in seconds if text word count is less than 200' do
-      expect(estimated_reading_time(150)).to eq 45
-      expect(estimated_reading_time(100)).to eq 30
-      expect(estimated_reading_time(87)).to eq 26.1 
-      expect(estimated_reading_time(20)).to eq 6
-      expect(estimated_reading_time(10)).to eq 3
+      expect(estimated_reading_time(200)).to eq "60 seconds"
+      expect(estimated_reading_time(150)).to eq "45 seconds"
+      expect(estimated_reading_time(100)).to eq "30 seconds"
+      expect(estimated_reading_time(87)).to eq "26 seconds"
+      expect(estimated_reading_time(20)).to eq "6 seconds"
+      expect(estimated_reading_time(10)).to eq "3 seconds"
     end
   end
 
