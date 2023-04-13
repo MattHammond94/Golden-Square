97172bb9ee02609069e25bbb283a94c3aca54c59
Matt Hammond <mjhammond94@gmail.com>
2023-04-13 12:12:14 +0100

    fail tests added for grammar checker

--- my_project/lib/grammar_checker.rb
+++ my_project/lib/grammar_checker.rb
@@ -1,4 +1,5 @@
 def grammar_checker(text)
+  fail "This is not a valid input type" unless text.is_a?(String)
   if text.start_with?(/[A-Z]/) && text.end_with?(".", "!", "?")
     true
   else
--- my_project/spec/grammar_checker_spec.rb
+++ my_project/spec/grammar_checker_spec.rb
@@ -10,6 +10,20 @@ describe 'GrammarChcecker' do
   end
 
   it 'Should return false when text does not end in correct grammar' do
-    expect(grammar_checker("This is incorrect]>")).to eq true
+    expect(grammar_checker("This is incorrect]>")).to eq false
+  end
+
+  it "Should return false for an empty string" do 
+    expect(grammar_checker("")).to eq false
+    expect(grammar_checker(" ")).to eq false
+    expect(grammar_checker("      ")).to eq false
+  end
+
+  context 'failures' do 
+    it 'Should return an error if anything other than a string is passed' do
+      expect { grammar_checker(:symbol) }.to raise_error "This is not a valid input type"
+      expect { grammar_checker(9) }.to raise_error "This is not a valid input type"
+      expect { grammar_checker(true) }.to raise_error "This is not a valid input type"
+    end
   end
 end
