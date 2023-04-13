98b16138317d4eeb349e2437811e49aaea37408d
Matt Hammond <mjhammond94@gmail.com>
2023-04-13 10:16:38 +0100

    spec added and file updated to account for seconds

--- my_project/lib/estimated_reading_time.rb
+++ my_project/lib/estimated_reading_time.rb
@@ -1,4 +1,8 @@
 def estimated_reading_time(text)
-  fail "You have not entered a valid amount of words" if text.nil?
-  text.to_f / 200
+  fail "You have not entered a valid amount of words" if text.nil? 
+  if text < 200 
+    (text.to_f / 200) * 60
+  else 
+    text.to_f / 200
+  end
 end
--- my_project/spec/estimated_reading_time_spec.rb
+++ my_project/spec/estimated_reading_time_spec.rb
@@ -1,6 +1,7 @@
 require 'estimated_reading_time'
 
 describe 'EstimatedReadingTime' do
+
   it 'Should return 1 for a total of 200 words' do
     expect(estimated_reading_time(200)).to eq 1
   end
@@ -9,19 +10,25 @@ describe 'EstimatedReadingTime' do
     expect(estimated_reading_time(400)).to eq 2
   end
 
-  it 'Should return 0.5 for a total of 100 words' do
-    expect(estimated_reading_time(100)).to eq 0.5
+  context 'word counts less than 200' do
+    it 'Should return a value in seconds if text word count is less than 200' do
+      expect(estimated_reading_time(150)).to eq 45
+      expect(estimated_reading_time(100)).to eq 30
+      expect(estimated_reading_time(87)).to eq 26.1 
+      expect(estimated_reading_time(20)).to eq 6
+      expect(estimated_reading_time(10)).to eq 3
+    end
   end
 
-  it 'Should return 0.1 for a total of 20 words' do
-    expect(estimated_reading_time(20)).to eq 0.1
+  context 'no amount of words' do
+    it 'Should return a 0 for no words' do
+      expect(estimated_reading_time(0)).to eq 0
+    end
   end
-
-  it 'Should return a 0 for no words' do
-    expect(estimated_reading_time(0)).to eq 0
-  end
-
-  it 'Should return an error if nothing is passed as an arg' do
-    expect { estimated_reading_time(nil) }.to raise_error "You have not entered a valid amount of words"
+  
+  context 'Should return an error for invalid inputs' do
+    it 'fails' do
+      expect { estimated_reading_time(nil) }.to raise_error "You have not entered a valid amount of words"
+    end
   end
 end
