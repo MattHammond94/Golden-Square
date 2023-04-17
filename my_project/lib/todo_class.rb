fb7f43b3a5e5015ca59f1dcb7f63b044579ff6b0
Matt Hammond <mjhammond94@gmail.com>
2023-04-17 10:35:55 +0100

    files updated - first test passed

--- my_project/lib/todo_class.rb
+++ my_project/lib/todo_class.rb
@@ -1,13 +1,18 @@
 class TodoTracker
-  def initialize 
+  def initialize(title)
+    @title = title
+    @list = []
   end
 
   def add(task)
+
   end
 
   def list
+
   end
 
   def mark_as_complete(task)
+
   end
 end
---
+++ my_project/spec/todo_class_spec.rb
@@ -0,0 +1,9 @@
+require 'todo_class'
+
+describe TodoTracker do 
+  it 'Should create a new todo list' do
+    todo_1 = TodoTracker.new("Aprils tasks")
+  end
+
+
+end
