8bbc8f98aa75f0ba7254a308d3cf97d722a21f1e
Matt Hammond <mjhammond94@gmail.com>
2023-04-17 10:47:39 +0100

    Third test added - program updated - third test passed

--- my_project/lib/todo_class.rb
+++ my_project/lib/todo_class.rb
@@ -13,6 +13,7 @@ class TodoTracker
   end
 
   def mark_as_complete(task)
-
+    location = @list.index(task)
+    @list.delete_at(location)
   end
 end
--- my_project/spec/todo_class_spec.rb
+++ my_project/spec/todo_class_spec.rb
@@ -9,6 +9,14 @@ describe TodoTracker do
       todo_1.add("Update my ipod")
       expect(todo_1.list).to eq ["Buy a new knuckleduster", "Polish said knuckleduster", "Update my ipod"]
     end
-  end
 
+    it 'Should return a shortened list when mark_as_complete has been called' do
+      todo_1 = TodoTracker.new("Aprils tasks")
+      todo_1.add("Buy a new knuckleduster")
+      todo_1.add("Polish said knuckleduster")
+      todo_1.add("Update my ipod")
+      todo_1.mark_as_complete("Polish said knuckleduster")
+      expect(todo_1.list).to eq ["Buy a new knuckleduster", "Update my ipod"]
+    end
+  end
 end
