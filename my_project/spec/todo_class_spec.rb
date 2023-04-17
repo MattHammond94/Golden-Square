a4007423d0f956e74a0c1c943350925eb99fb604
Matt Hammond <mjhammond94@gmail.com>
2023-04-17 10:57:43 +0100

    extended test added

--- my_project/spec/todo_class_spec.rb
+++ my_project/spec/todo_class_spec.rb
@@ -19,4 +19,20 @@ describe TodoTracker do
       expect(todo_1.list).to eq ["Buy a new knuckleduster", "Update my ipod"]
     end
   end
+
+  context 'Multiple tasks added and removed' do
+    it 'Should return a list as expected after multiple adds followed by multiple completes and vice versa' do
+      todo_1 = TodoTracker.new("Aprils tasks")
+      todo_1.add("Buy a new knuckleduster")
+      todo_1.add("Polish said knuckleduster")
+      todo_1.add("Update my ipod")
+      todo_1.add("Plant a cherry tree in the garden")
+      todo_1.add("Feed the peregrine falcon")
+      todo_1.mark_as_complete("Update my ipod")
+      todo_1.add("Complete my short novella")
+      todo_1.mark_as_complete("Buy a new knuckleduster")
+      todo_1.add("Make a batch of brownies")
+      expect(todo_1.list).to eq ["Polish said knuckleduster", "Plant a cherry tree in the garden", "Feed the peregrine falcon", "Complete my short novella", "Make a batch of brownies"]
+    end
+  end
 end
