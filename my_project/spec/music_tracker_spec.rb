00e5cf8ed46696ff9c5012a3c97851ae178b81ae
Matt Hammond <mjhammond94@gmail.com>
2023-04-17 20:35:47 +0100

    tests updated

--- my_project/spec/music_tracker_spec.rb
+++ my_project/spec/music_tracker_spec.rb
@@ -8,6 +8,7 @@ describe MusicTracker do
       library.add("Bicep - Glue")
       library.add("Tool - Lateralus")
       library.add("Sex Pistols - No Fun")
+      library.add("")
       expect(library.list).to eq ["James Blunt - You're Beautiful", "Bicep - Glue", "Tool - Lateralus", "Sex Pistols - No Fun"]
     end
   end
@@ -24,6 +25,8 @@ describe MusicTracker do
     it 'Should return an error message when add is passed anything other than a string' do
       library = MusicTracker.new
       expect { library.add(12) }.to raise_error "Invalid input type"
+      expect { library.add(nil) }.to raise_error "Invalid input type"
+      expect { library.add (:symbol) }.to raise_error "Invalid input type"
     end
   end
 end
