8f325ebb984c8680a62d6fa75b444d302f88af00
Matt Hammond <mjhammond94@gmail.com>
2023-04-17 20:20:27 +0100

    music tracker spec and lib file updated - tests passed

--- my_project/lib/music_tracker.rb
+++ my_project/lib/music_tracker.rb
@@ -1,4 +1,14 @@
 class MusicTracker 
   def initialize
+    @library = []
+  end
+
+  def add(track)
+    fail "Invalid input type" unless track.is_a?(String)
+    track.empty? ? nil : @library << track
+  end
+
+  def list
+    @library
   end
 end
--- my_project/spec/music_tracker_spec.rb
+++ my_project/spec/music_tracker_spec.rb
@@ -1,5 +1,29 @@
 require 'music_tracker'
 
 describe MusicTracker do
-  
+  context 'Multiple add method calls' do
+    it 'Should return a list of tracks after multiple add methods are called' do
+      library = MusicTracker.new 
+      library.add("James Blunt - You're Beautiful")
+      library.add("Bicep - Glue")
+      library.add("Tool - Lateralus")
+      library.add("Sex Pistols - No Fun")
+      expect(library.list).to eq ["James Blunt - You're Beautiful", "Bicep - Glue", "Tool - Lateralus", "Sex Pistols - No Fun"]
+    end
+  end
+
+  context 'Empty Strings' do
+    it 'Should return an empty list if an empty string is passed to add before calling list' do
+      library = MusicTracker.new
+      library.add("")
+      expect(library.list).to eq []
+    end
+  end
+
+  context 'Failures' do 
+    it 'Should return an error message when add is passed anything other than a string' do
+      library = MusicTracker.new
+      expect { library.add(12) }.to raise_error "Invalid input type"
+    end
+  end
 end
