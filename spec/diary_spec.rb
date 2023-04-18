ef7f7c7ae5fd3b75eb681afd6991863750ea98b3
Matt Hammond <mjhammond94@gmail.com>
2023-04-18 10:58:34 +0100

    test moved to integration spec

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -2,15 +2,11 @@ require 'diary'
 
 describe Diary do
   context 'builds a list of all entries' do
-    it 'Should return a list of entries after multiple diary entries have been created' do
+    it 'Should return a list of entries after multiple diary adds have been called' do
       diary_1 = Diary.new
-      entry_1 = DiaryEntry.new("April", "Went to the shop")
-      entry_2 = DiaryEntry.new("May", "Went to the shops again nothing major")
-      entry_3 = DiaryEntry.new("June", "Went to the beach, the shops were closed")
-      diary_1.add(entry_1)
-      diary_1.add(entry_2)
-      diary_1.add(entry_3)
-      expect(diary_1.all).to eq [entry_1, entry_2, entry_3]
+      diary_1.add("Random entry")
+      diary_1.add("Random entry the second yes")
+      expect(diary_1.all).to eq ["Random entry", "Random entry the second yes"]
     end
   end
 end
