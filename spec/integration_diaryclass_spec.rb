70dd515d9e5824ae0928b157e888b3f278bbc785
Matt Hammond <mjhammond94@gmail.com>
2023-04-18 12:10:03 +0100

    integration test added

--- spec/integration_diaryclass_spec.rb
+++ spec/integration_diaryclass_spec.rb
@@ -14,4 +14,17 @@ describe 'Integration' do
       expect(diary_1.all).to eq [entry_1, entry_2, entry_3]
     end
   end
+
+  context 'returns a list of word counts' do
+    it 'Should return a list of word counts for each entry after multiple diary entries have been created' do
+      diary_1 = Diary.new
+      entry_1 = DiaryEntry.new("April", "Went to the shop")
+      entry_2 = DiaryEntry.new("May", "Went to the shops again nothing major")
+      entry_3 = DiaryEntry.new("June", "Went to the beach, the shops were closed")
+      diary_1.add(entry_1)
+      diary_1.add(entry_2)
+      diary_1.add(entry_3)
+      expect(diary_1.count_words).to eq [4, 7, 8]
+    end
+  end
 end
