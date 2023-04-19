4c33413b5937d0476285508acb19ab73873bdd01
Matt Hammond <mjhammond94@gmail.com>
2023-04-19 15:10:00 +0100

    fail tests added for todo entry - tests passed

--- designing_a_multi_class_program/spec/integeration_spec.rb
+++ designing_a_multi_class_program/spec/integeration_spec.rb
@@ -4,5 +4,8 @@ require 'diary_entry'
 
 describe Diary do
   context '' do
+    it 'Should' do
+      
+    end
   end
 end
--- designing_a_multi_class_program/spec/todo_entry_spec.rb
+++ designing_a_multi_class_program/spec/todo_entry_spec.rb
@@ -12,4 +12,18 @@ describe TodoEntry do
       expect(entry_3.title).to eq "March"
     end
   end
+
+  context 'Failures' do
+    it 'Should return an error message when TodoEntry is initialized without string values' do
+      expect { entry_2 = TodoEntry.new(nil, "One two three") }.to raise_error "Both inputs must be valid in order to create a new entry"
+      expect { entry_3 = TodoEntry.new("April", []) }.to raise_error "Both inputs must be valid in order to create a new entry"
+      expect { entry_4 = TodoEntry.new("June", 12) }.to raise_error "Both inputs must be valid in order to create a new entry"
+    end
+
+    it 'Should return an error message when TodoEntry is initialized with empty strings' do
+      expect { entry_1 = TodoEntry.new("", "") }.to raise_error "One of your inputs is empty - Please try again"
+      expect { entry_5 = TodoEntry.new("June", "") }.to raise_error "One of your inputs is empty - Please try again"
+      expect { entry_5 = TodoEntry.new("", "one two three") }.to raise_error "One of your inputs is empty - Please try again"
+    end
+  end
 end
