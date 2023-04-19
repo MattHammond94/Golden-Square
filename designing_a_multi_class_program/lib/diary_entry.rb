ac8bf27c141c3e3d3f05e0e5d3510ffed0d49c5b
Matt Hammond <mjhammond94@gmail.com>
2023-04-19 14:52:42 +0100

    fail tests added - diary entry program updated - tests passed

--- designing_a_multi_class_program/lib/diary_entry.rb
+++ designing_a_multi_class_program/lib/diary_entry.rb
@@ -1,7 +1,13 @@
 class DiaryEntry
   def initialize(title, contents)
+    if !(title.is_a?(String)) || !(contents.is_a?(String))
+      fail "Both inputs must be valid in order to create a new entry"
+    elsif title.empty? || contents.empty? 
+      fail "One of your inputs is empty - Please try again"
+    else
     @title = title
     @contents = contents
+    end
   end
 
   def title
--- designing_a_multi_class_program/spec/diary_entry_spec.rb
+++ designing_a_multi_class_program/spec/diary_entry_spec.rb
@@ -8,8 +8,22 @@ describe DiaryEntry do
       entry_3 = DiaryEntry.new("Another one", "DJ Khaled")
       entry_1.title => "Yes"
       expect(entry_2.contents).to eq "This is the second test"
-      # entry_3.contets => "DJ Khaled"
-      # entry_3.title => "Another one"
+      expect(entry_3.contents).to eq "DJ Khaled"
+      expect(entry_3.title).to eq "Another one"
+    end
+  end
+
+  context 'Failures' do
+    it 'Should return an error message when DiaryEntry is initialized without string values' do
+      expect { entry_2 = DiaryEntry.new(nil, "One two three") }.to raise_error "Both inputs must be valid in order to create a new entry"
+      expect { entry_3 = DiaryEntry.new("April", []) }.to raise_error "Both inputs must be valid in order to create a new entry"
+      expect { entry_4 = DiaryEntry.new("June", 12) }.to raise_error "Both inputs must be valid in order to create a new entry"
+    end
+
+    it 'Should return an error message when DiaryEntry is initialized with empty strings' do
+      expect { entry_1 = DiaryEntry.new("", "") }.to raise_error "One of your inputs is empty - Please try again"
+      expect { entry_5 = DiaryEntry.new("June", "") }.to raise_error "One of your inputs is empty - Please try again"
+      expect { entry_5 = DiaryEntry.new("", "one two three") }.to raise_error "One of your inputs is empty - Please try again"
     end
   end
 end
