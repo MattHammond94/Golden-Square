b5d06259571198239e68badf8394c6a5ee6e6d19
Matt Hammond <mjhammond94@gmail.com>
2023-04-20 12:18:10 +0100

    first test updated

--- phase_three/spec/music_library_spec.rb
+++ phase_three/spec/music_library_spec.rb
@@ -5,8 +5,10 @@ describe MusicLibrary do
     it 'Should' do
       library_1 = MusicLibrary.new
       fake_track = FakeTrack.new
+      fake_track_2 = FakeTrack.new
       library_1.add(fake_track)
-      expect(library_1.all).to eq [fake_track]
+      library_1.add(fake_track_2)
+      expect(library_1.all).to eq [fake_track, fake_track_2]
     end
   end
 end
