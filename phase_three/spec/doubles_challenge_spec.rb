fe2cf77a0c4324fd7dd098dc37af0999c4d5c541
Matt Hammond <mjhammond94@gmail.com>
2023-04-21 10:43:43 +0100

    Challenge solution updated

--- phase_three/spec/doubles_challenge_spec.rb
+++ phase_three/spec/doubles_challenge_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "doubles challenge" do
 
     task_list.add(task)
     
-    allow(task_list).to receive(:list).and_return [task]
-    allow(task_list).to receive(:count).and_return 1
-    allow(task_list).to receive(:clear).and_return :success
+    expect(task_list).to receive(:list).and_return [task]
+    expect(task_list).to receive(:count).and_return 1
+    expect(task_list).to receive(:clear).and_return :success
 
     expect(task_list.list).to eq [task]
     expect(task_list.count).to eq 1
