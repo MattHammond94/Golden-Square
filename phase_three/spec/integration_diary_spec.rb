d6f607ca9b1d2d7726f9e00bfa4b31bc08a5dd53
Matt Hammond <mjhammond94@gmail.com>
2023-04-21 12:00:59 +0100

    Exercise completed

--- phase_three/spec/integration_diary_spec.rb
+++ phase_three/spec/integration_diary_spec.rb
@@ -2,9 +2,16 @@ require 'secret_diary'
 require 'diary'
 
 describe 'integration' do
-  it 'Should not read the diary because it is locked' do
+  it 'Should not read the diary when locked' do
     diary = Diary.new("contents of this diary are secret")
     secret_diary = SecretDiary.new(diary)
     expect { secret_diary.read }.to raise_error "Go away!"
+  end
+
+  it 'Should read diary entry when unlocked' do
+    diary = Diary.new("contents of this diary are secret")
+    secret_diary = SecretDiary.new(diary)
+    secret_diary.unlock
+    expect(secret_diary.read).to eq "contents of this diary are secret"
   end
 end
