bab8ce05b3a370bfcc6c3749f738da491eaeb0de
Matt Hammond <mjhammond94@gmail.com>
2023-04-28 14:56:43 +0100

    interactive calculator tests passed

--- phase_three/lib/interactive_calculator.rb
+++ phase_three/lib/interactive_calculator.rb
@@ -1,10 +1,23 @@
-interactive_calculator = InteractiveCalculator.new(Kernel)
-interactive_calculator.run
+class InteractiveCalculator
 
-# Hello. I will subtract two numbers.
-# Please enter a number
-# 4
-# Please enter another number
-# 3
-# Here is your result:
-# 4 - 3 = 1
+  def initialize(terminal)
+    @terminal = terminal
+  end
+
+  def run
+    @terminal.puts "Hello, I will subtract two numbers."
+    @terminal.puts "Please enter a number"
+    input_1 = @terminal.gets.chomp
+    fail "Input must be a valid integer." unless input_1.match(/\d/)
+    @terminal.puts "Please enter another number"
+    input_2 = @terminal.gets.chomp
+    fail "Input must be a valid integer." unless input_2.match(/\d/)
+    @terminal.puts "Here is your result:"
+    result = input_1.to_i - input_2.to_i
+    @terminal.puts "#{input_1} - #{input_2} = #{result}"
+  end
+end
+
+
+# interactive_calculator = InteractiveCalculator.new(Kernel)
+# interactive_calculator.run
--- phase_three/spec/interactive_calculator_spec.rb
+++ phase_three/spec/interactive_calculator_spec.rb
@@ -1,2 +1,71 @@
 require 'interactive_calculator'
 
+describe InteractiveCalculator do
+  it 'Subtracts two given numbers' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("10").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("10 - 4 = 6").ordered
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    interactive_calculator.run
+  end
+
+  it 'Should subtract two numbers and return a negative value' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("10").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("13").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("10 - 13 = -3").ordered
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    interactive_calculator.run
+  end
+
+  # it 'Should subtract two floating point numbers' do
+  #   terminal = double :io
+  #   expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+  #   expect(terminal).to receive(:puts).with("Please enter a number").ordered
+  #   expect(terminal).to receive(:gets).and_return("0.9").ordered
+  #   expect(terminal).to receive(:puts).with("Please enter another number").ordered
+  #   expect(terminal).to receive(:gets).and_return("0.2").ordered
+  #   expect(terminal).to receive(:puts).with("Here is your result:").ordered
+  #   expect(terminal).to receive(:puts).with("0.9 - 0.2 = 0.7").ordered
+  #   interactive_calculator = InteractiveCalculator.new(terminal)
+  #   interactive_calculator.run
+  # end
+
+  it 'Should fail/error when given a value thats not an integer' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("Sandwich").ordered
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    expect { interactive_calculator.run }.to raise_error "Input must be a valid integer."
+  end
+
+  it 'Should fail/error when given a value thats not an integer' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("[..]/").ordered
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    expect { interactive_calculator.run }.to raise_error "Input must be a valid integer."
+  end
+
+  it 'Should fail/error when given a value thats not an integer as the second input' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello, I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("12").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("String").ordered
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    expect { interactive_calculator.run }.to raise_error "Input must be a valid integer."
+  end
+end
