e3a714d407729e3592d28bae9ce8aad0426a7d00
Matt Hammond <mjhammond94@gmail.com>
2023-04-28 17:51:37 +0100

    Fail test added

--- phase_three/spec/string_repeater_spec.rb
+++ phase_three/spec/string_repeater_spec.rb
@@ -13,4 +13,30 @@ describe StringRepeater do
     repeater = StringRepeater.new(terminal)
     repeater.run
   end
+
+  it 'Should repeat given string given amount of times' do
+    terminal = double :io
+    expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a string").ordered
+    expect(terminal).to receive(:gets).and_return("Salmon").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("SalmonSalmonSalmon").ordered
+    repeater = StringRepeater.new(terminal)
+    repeater.run
+  end
+
+  context 'fails/errors' do
+    it 'return an error message when a negative int is given ' do
+      terminal = double :io
+      expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+      expect(terminal).to receive(:puts).with("Please enter a string").ordered
+      expect(terminal).to receive(:gets).and_return("Salmon").ordered
+      expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
+      expect(terminal).to receive(:gets).and_return("-3").ordered
+      repeater = StringRepeater.new(terminal)
+      expect { repeater.run }.to raise_error "Integer must be a postive number."
+    end
+  end
 end
